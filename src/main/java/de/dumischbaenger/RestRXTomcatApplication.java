b941bd6a68d1ab8564493b8dd6ebb74235ac9cca
bd <bd@brix.fritz.box>
2020-01-28 19:58:45 +0100

    added Application object

---
+++ src/main/java/de/dumischbaenger/RestRXTomcatApplication.java
@@ -0,0 +1,9 @@
+package de.dumischbaenger;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("apppath")
+public class RestRXTomcatApplication extends Application {
+
+}
