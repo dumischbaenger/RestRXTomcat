a1db82570a23bcf018f99773e8a57505002bbed1
bd <bd@brix.fritz.box>
2020-01-28 21:05:21 +0100

    web.xml without application class

--- src/main/java/de/dumischbaenger/RestRXTomcatApplication.java
+++
@@ -1,9 +0,0 @@
-package de.dumischbaenger;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("apppath")
-public class RestRXTomcatApplication extends Application {
-
-}
