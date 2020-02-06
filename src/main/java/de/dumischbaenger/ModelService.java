2e550edf7ff688b5113aaf11dbd30ef2a319f760
bd <bd@brix.fritz.box>
2020-02-06 21:06:34 +0100

    added service class

---
+++ src/main/java/de/dumischbaenger/ModelService.java
@@ -0,0 +1,37 @@
+package de.dumischbaenger;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+
+@SessionScoped
+@Named("modelService")
+public class ModelService implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  public ModelService() {
+    super();
+  }
+
+  public List<ModelClass> getModelList() {
+    List<ModelClass> list=new LinkedList<>();
+
+    ModelClass m=new ModelClass();
+    m.setName("hello world1");
+    list.add(m);
+
+    m=new ModelClass();
+    m.setName("hello world2");
+    list.add(m);
+
+    return list;
+  }
+  
+  
+
+  
+ }
--- src/main/java/de/dumischbaenger/RestTestService.java
+++ src/main/java/de/dumischbaenger/RestTestService.java
@@ -1,5 +1,8 @@
 package de.dumischbaenger;
 
+import java.util.List;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,12 +11,16 @@ import javax.ws.rs.core.MediaType;
 @Path("/resttest")
 public class RestTestService {
 
+  @Inject
+  ModelService service;
+  
   @GET
   @Path("/modelclass")
   @Produces({MediaType.APPLICATION_JSON,MediaType.APPLICATION_XML})
-	public ModelClass getModellClass() {
-    ModelClass m=new ModelClass();
-    m.setName("hello world");
-	  return m;
+	public List<ModelClass> getModellClass() {
+    List<ModelClass> list=service.getModelList();
+//    ModelClass m=new ModelClass();
+//    m.setName("hello world");
+	  return list;
 	}
 }
